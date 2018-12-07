c8b00fd4771548f2ece17fe43b30ade6a8162a27
Jannat Binta Alam <jannat1ruma@gmail.com>
2018-12-07 14:10:42 +0600

    Add files via upload

---
+++ Array_String.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            stringt();
+            //arr()
+            Console.ReadKey();
+        }
+        public static void arr()
+        {
+            //array is a reference type data
+            //dicliar a aray
+            int[] arr1 = new int[5];//1st way
+            //Array a = new Array[5];
+            int[] arr2 = new int[5] { 0, 1, 3, 4, 5 };//2nd way
+            int[] arr3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, };//3rd way
+
+            foreach (int a in arr2)
+            {
+                Console.WriteLine(a);
+            }
+
+            //multidimentional array
+            int[,] mular1 = new int[2, 3];//1st way
+            int[,] mular2 = new int[,] { { 10, 29 }, { 30, 40 } };
+            mular1[0, 1] = 10;
+
+
+            //variable[ row , column ] = values
+            //jagged arrayes- also called arrayes of array
+
+            int[][] array2 = new int[3][];
+            array2[0] = new int[3] { 1, 2, 3 };
+            array2[1] = new int[] { 4, 5, 6, 7, 8, 9 };
+            array2[2] = new int[10];
+            // Console.WriteLine(array2[0][1]);//2
+
+
+
+            //Console.WriteLine(mular1[0, 1]);
+            Console.ReadKey();
+
+
+        }
+        public static void stringt()
+        {
+            //string cancating
+            string firstname, lastname;
+            firstname = "Mushfiqur";
+            lastname = "Rahman";
+            string name = firstname +" "+ lastname;//
+            Console.WriteLine(name);
+            string fulname =  String.Concat(firstname,"+", lastname);
+            Console.WriteLine(fulname);
+            Console.WriteLine("Hello {0}", firstname + " " + lastname);
+            Console.WriteLine("how are you " + firstname + " " + lastname +" ? ");
+
+            // by using string constractor
+            char[] letters = { 'H', 'E', 'L', 'L', 'O' };
+            string st = new string(letters);
+            Console.WriteLine("Hello " + st);
+
+            //for convert string to arr
+            char[] let = st.ToCharArray();
+            foreach (char a in let)
+            {
+                Console.WriteLine(a);
+            }
+
+            //methods returning string
+
+            string[] words = { "how", "are", "you", "?" };
+            string sentence = String.Join(" ", words);
+            Console.WriteLine(sentence);
+
+
+
+            //upper lower case in string
+            string sentences = "heLlo WoRld";
+            string lower = sentences.ToLower();
+            string upper = sentences.ToUpper();
+            Console.WriteLine("Upper " + upper + " Lower " + lower);
+
+            //perticular scarch
+            string bd = "I love my country very much";
+            Console.WriteLine(bd.StartsWith("I"));//if start with I it will return true either false
+            Console.WriteLine(bd.EndsWith("h"));//if ends with with h it willl return true eiher false
+            Console.WriteLine(bd.Contains("very"));
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Basic_syntax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console is a  class of system namespace
+
+            //Console.BackgroundColor = ConsoleColor.DarkBlue;
+            //backgroungColor property set background color 
+
+           // Console.ForegroundColor = ConsoleColor.Green;
+            //ForegroundColor set text color
+
+            //ConsoleColor col = Console.BackgroundColor;
+            //ConsoleColor col1 = Console.ForegroundColor;
+
+            //Console.CursorSize = 100;
+            //set cursor depth
+
+            //Console.Beep(2100,1000);
+            //beep(int frequency,int milisecond)
+
+            //Console.WriteLine(col);
+            //Console.Write(col1);
+            //Writeline can write any variables in n line 
+
+           // Console.WriteLine(100);
+
+           Console.SetWindowSize(200, 50);
+            //SetWindowSize(width,height);
+
+           Console.Write(20008978675);
+
+            //Console.Clear();
+            //clear will clear the screan
+
+            Console.ReadKey();
+            //Console.ReadKey();
+
+            //waiiting for a key
+        }
+    }
+}
